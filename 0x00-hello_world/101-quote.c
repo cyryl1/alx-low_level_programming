f5e7690a0992abfc337fd6e703e9cdd1630142b9
Wise1m <aribisalapraise12@gmail.com>
2023-02-17 08:55:03 -0800

    Committed file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
-#insert <unistd.h>
-
-int main()
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: program structure
+ *
+ * Return: Always 1 (unsuccessful)
+ */
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return 1;
+	return (1);
 }
