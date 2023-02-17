5acbb19576e3a513f57fdc1ca25482d7e77ebf24
Wise1m <aribisalapraise12@gmail.com>
2023-02-17 08:25:32 -0800

    Committed file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
-int main() 
+/**
+ * main - Entry point
+ *
+ * Description: displaying a text using printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
 {
-	printf("%s%s%s\n", "with proper grammar,", "but the outcome is a piece of art", ",");
-	return 0;
+	printf("%s%s%s\n", "with proper grammar, ", "but the outcome is a piece of art", ",");
+	return (0);
 }
