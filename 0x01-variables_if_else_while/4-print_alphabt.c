f86c2728b218900066bd5020ebc453af250c8fe5
Wise1m <aribisalapraise12@gmail.com>
2023-02-17 20:58:10 -0800

    Committed file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- Entry Point
+ *
+ * Description: print all alpabet except q and e
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void) {
+	char letters = 'a';
+
+	while (letters <= 'z')
+	{
+		if (letters != 'e' && letters != 'q')
+		{
+			putchar(letters);
+		}
+		letters++;
+	}
+	putchar('\n')
+	retun (0);
