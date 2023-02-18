2720c375ab055057b40c9987e1daf3e7a6bc351c
Wise1m <aribisalapraise12@gmail.com>
2023-02-17 20:06:04 -0800

    Committed file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,9 +17,11 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive", n);
-	}else if (n == 0){
+	} else if (n == 0)
+	{
 		printf("%d is zero", n);
-	}else {
+	} else 
+	{
 		printf("%d is negative", n);
 	}
 
