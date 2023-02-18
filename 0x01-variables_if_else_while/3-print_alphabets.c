ab421421837ce6329a8efc925b6698eb7df59eec
Wise1m <aribisalapraise12@gmail.com>
2023-02-17 19:39:18 -0800

    Committed file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Description: To prin both lowercase and uppercase letters
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char lower_case = 'a';
+	char upper_case = 'A';
+
+	do {
+		putchar(lower_case);
+		lower_case++;
+	} while (lower_case <= 'z');
+
+	do {
+		putchar(upper_case);
+		upper_case++;
+	} while(upper_case <= 'Z');
+
+	putchar(\n);
+
+	return (0);
+}
