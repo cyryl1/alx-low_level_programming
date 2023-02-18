3a9aae918ce0b392db093452371c55876d046c51
Wise1m <aribisalapraise12@gmail.com>
2023-02-17 21:25:32 -0800

    Committed file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: to print numbers from 0 to 10
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		int putchar(num);
+	}
+
+	putchar(\n);
+	return (0);
+}
