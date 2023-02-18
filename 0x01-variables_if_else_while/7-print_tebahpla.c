d89aa575c3d5ba851ce6b45bae70a0dee9e08b11
Wise1m <aribisalapraise12@gmail.com>
2023-02-17 21:39:14 -0800

    Committed file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point 
+* 
+* Description: to print alphabet in reverse
+* 
+* Return: Always 0 (Success) 
+*/
+
+int main(void)
+{
+	char reverse_letters = 'z';
+
+	while (reverse_letters >= 'a')
+	{
+		putchar(reverse_letters);
+		reverse_letters--;
+	}
+	putchar('\n');
+	return (0);
+}
+
