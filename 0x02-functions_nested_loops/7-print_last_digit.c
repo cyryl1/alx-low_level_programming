f2bf99ee08cedb4d67865bf6836b8fcf44279dbb
Wise1m <aribisalapraise12@gmail.com>
2023-02-21 14:15:59 -0800

    committed file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,10 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
+	int last_digit = n % 10;
 
-	if (n < 0)
-		last_digit = -1 * (n % 10);
-	else
-		last_digit = n * 10;
+	if (last_digit < 0)
+		last_digit *= -1;
 
 	_putchar(last_digit + '0');
 
