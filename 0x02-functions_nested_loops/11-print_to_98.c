006cf3e62cbde9daa943c3be144b8b2ca93310c3
Wise1m <aribisalapraise12@gmail.com>
2023-02-21 15:48:49 -0800

    committed file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_to_98 - to print all natural numbers to 98
+ * @n: the number to be printed from
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		if (i = n; i < 98; i++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		if (i = n; i > 98; i--)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
