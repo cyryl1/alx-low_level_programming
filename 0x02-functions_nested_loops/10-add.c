3be6fb3eca0900f6f71b1114b1bd901e805731c7
Wise1m <aribisalapraise12@gmail.com>
2023-02-21 15:24:36 -0800

    committed file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
  * add - to add two integers
- *
+ *@a: integer 1
+ @b: integer 2
+
  * Return: always 0 (success)
  */
 int add(int a, int b)
 {
 	int c;
+
 	c = a + b;
 	_putchar(c);
 
