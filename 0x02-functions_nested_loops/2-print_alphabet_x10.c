d3385a44467ba367fdd48f14668787d9ba3a6bb5
Wise1m <aribisalapraise12@gmail.com>
2023-02-21 12:27:22 -0800

    committed file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10: To print lowercase alphabets 10 times
+ * print_alphabet_x10 - To print lowercase alphabets 10 times
  */
 
 void print_alphabet_x10(void)
@@ -20,10 +20,3 @@ void print_alphabet_x10(void)
 		alphabets++;
 	}
 }
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
