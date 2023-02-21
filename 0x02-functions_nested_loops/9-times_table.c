1f1ffe1559433c557a1d238927dcfa60e172c9bb
Wise1m <aribisalapraise12@gmail.com>
2023-02-21 15:14:55 -0800

    committed file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,19 @@ void times_table(void)
 			product = i * j;
 
 			if (j == 0)
+			{
 				_putchar('0');
+			}
 			else if (product < 10)
+			{
 				_putchar(' ');
 				_putchar('0' + product);
+			}
 			else
-				_putchar('0' + product / 10);
-				_putchar('0' + product % 10);
+			{
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 1)0);
+			}
 
 			if (j != 9)
 			{
