fe2038a79265495f1933858fb94d4eb64e1cf880
Wise1m <aribisalapraise12@gmail.com>
2023-02-22 13:36:34 -0800

    committed file

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 50, a = 1, b = 2, i, c;
+	printf("%d, %d, ", a, b);
+
+	for (i = 3; i <= n; i++)
+	{
+		c = a + b;
+		printf("%d", c);
+
+		if (i < n)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+		a = b;
+		b = c;
+	}
+	return (0);
+}
