a286a89d9cff5e4dda758fb7376ab98c4c28877c
Wise1m <aribisalapraise12@gmail.com>
2023-02-21 22:02:20 -0800

    committed file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,40 +1,40 @@
 #include "main.h"
 /**
  * print_to_98 - to print all natural numbers to 98
- * @n: the number to be printed from
+ * @n: the number to be printed
  */
 void print_to_98(int n)
 {
-    while (n != 98) {
-        /* print current number */
-        if (n < 0) {
-            _putchar('-');
-            n = -n;
-        }
-        if (n >= 10)
-            print_to_98(n / 10);
-        _putchar(n % 10 + '0');
-
-        /* print separator */
-        _putchar(',');
-        _putchar(' ');
-
-        /* update number */
-        if (n < 98)
-            n++;
-        else
-            n--;
-    }
-
-    /* print last number */
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n >= 10)
-        print_to_98(n / 10);
-    _putchar(n % 10 + '0');
-
-    /* print newline */
-    _putchar('\n');
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
 }
