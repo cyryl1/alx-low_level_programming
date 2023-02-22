7c5c05bbbea7f1c2917807e08f2d9046dfe55440
Wise1m <aribisalapraise12@gmail.com>
2023-02-21 16:00:32 -0800

    committed file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,8 +11,8 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			_putchar((i / 1)0 + '0');
-			_putchar((i % 1)0 + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i > 98; i--)
 		{
-			_putchar((i / 1)0 + '0');
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
