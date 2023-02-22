f28b574a663c419a194d19bd6a31b971bf3aff5f
Wise1m <aribisalapraise12@gmail.com>
2023-02-21 21:46:47 -0800

    committed file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,15 +3,6 @@
  * print_to_98 - to print all natural numbers to 98
  * @n: the number to be printed from
  */
-#include "holberton.h"
-#include <stdio.h>
-
-/**
-  * print_to_98 - Prints all natural numbers from n to 98
-  * @n: The number to start printing from
-  *
-  * Return: Always 0.
-  */
 void print_to_98(int n)
 {
 	if (n <= 98)
