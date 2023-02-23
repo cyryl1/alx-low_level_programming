debc88983b5a23e2457a5f9507ce6f753cb4aae2
Wise1m <aribisalapraise12@gmail.com>
2023-02-23 07:00:56 -0800

    File commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - to print 10 times numbers from 0 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i <= 10)
+	{
+		j = 0;
+
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
