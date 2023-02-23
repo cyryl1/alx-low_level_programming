8c0811756906802f4ea95981b0cfb789d44e12d1
Wise1m <aribisalapraise12@gmail.com>
2023-02-23 05:30:07 -0800

    File has been committed

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,25 +12,26 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	int daysInMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 	{
-		if (month > 2 && day >= 60)
-		{
-			day++;
-		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		daysInMonth[2] = 29;  // update February days for leap year
 	}
-	else
+	
+	if (month < 1 || month > 12 || day < 1 || day > daysInMonth[month])
 	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		return;
 	}
+	int dayOfYear = 0;
+	for (int i = 1; i < month; i++)
+	{
+		dayOfYear += daysInMonth[i];
+	}
+	dayOfYear += day;
+	
+	printf("Day of the year: %d\n", dayOfYear);
+	printf("Remaining days: %d\n", 365 + (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) - dayOfYear);
 }
+
