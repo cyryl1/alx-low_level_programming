b6610df5d6543600b5d5c8a3afeb4165c221fd5f
Wise1m <aribisalapraise12@gmail.com>
2023-02-24 05:31:30 -0800

    File commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,20 +11,20 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-		return;
-	}
-
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (i == n-1)
+				continue;
+			
+			_putchar('\n');
 		}
-	}	
-	_putchar('\\');
-	_putchar('\n');
 	}
+	
+	_putchar('\n');
 }
