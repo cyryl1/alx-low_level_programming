690ba7666da15c8c1a7da68a1774460b6ed80078
Wise1m <aribisalapraise12@gmail.com>
2023-02-24 06:07:10 -0800

    File commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,24 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-		return;
-	}
-
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= i; j++)
+		for (i = 1, i <= size; i++)
 		{
-			_putchar('#');
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			if (hash == size)
+			{
+				continue;
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
