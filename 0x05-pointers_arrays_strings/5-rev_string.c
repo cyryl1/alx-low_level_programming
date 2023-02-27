b01f11031494f9acf7d455ed76cd7f4da90cdb82
Wise1m <aribisalapraise12@gmail.com>
2023-02-27 07:46:07 -0800

    File committed

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - to reverse a string
+ * @s: pointer to use in function
+ */
+void rev_string(char *s)
+{
+	char temp;
+
+	int i = 0, j = strlen(s) - 1;
+	while(i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = trmp;
+		i++;
+		j--;
+	}
+}
