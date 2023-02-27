5f1d71317ae6a417eb7df7b5cc2675d628e934a6
Wise1m <aribisalapraise12@gmail.com>
2023-02-27 06:00:51 -0800

    File committed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98 - to take a pointer to an int as parameter and update the value it points to 98.
- * @n: the pointer used in the function.
+ * reset_to_98 - to take a pointer to an int as parameter and update the value
+ * @n: the pointer to be used
  */
 void reset_to_98(int *n)
 {
