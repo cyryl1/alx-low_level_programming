37b00b1af76a60144c409c129eb46c1d58301b0e
Wise1m <aribisalapraise12@gmail.com>
2023-02-27 02:55:39 -0800

    File committed

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array -  to print n elements of an array of integers
+ * @a: the pointer usd in function
+ * @n: igit to be printe
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (int i = 0; i < n; i++)
+	{
+		pintf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
