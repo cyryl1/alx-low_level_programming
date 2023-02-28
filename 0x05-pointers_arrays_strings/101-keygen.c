0e5e77b17a884af83787172ca04c36fe20918525
Wise1m <aribisalapraise12@gmail.com>
2023-02-27 18:02:04 -0800

    File committed

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0 always (success)
+ */
+int main (void)
+{
+	int i;
+	char password[10];
+	srand(time(NULL));
+
+	for (i = 0; i < 10; i++)
+	{
+		password[i] = rand() % 94 + 33;
+	}
+
+	printf("%s\n", password);
+	return (0);
+}
