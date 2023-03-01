7b2faa54f06f10b80f443db5111ff388da8f9266
Wise1m <aribisalapraise12@gmail.com>
2023-03-01 02:34:01 -0800

    Commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: maximum number of characters to copy
+ *
+ * Return: pointer to destination string
+ */
+char *_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
