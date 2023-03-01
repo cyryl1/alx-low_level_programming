40dc5d8f4d14e507562d1a8666c2471de286122f
Wise1m <aribisalapraise12@gmail.com>
2023-03-01 04:08:52 -0800

    Commit

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('_');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
