6f24f695acbd9f90e469ccf769d301d90ed95ed5
Wise1m <aribisalapraise12@gmail.com>
2023-03-01 02:22:43 -0800

    Commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings.
+ *
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of characters to concatenate.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+	int i = 0;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (i < n && *src != '\0')
+	{
+		*p++ = *src++;
+		i++;
+	}
+	
+	*p = '\0';
+	
+	return (dest);
+}
