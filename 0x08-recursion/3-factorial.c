d541f6e2a5a7e118eca36e4cb5b39b1b504ad410
Wise1m <aribisalapraise12@gmail.com>
2023-03-08 11:40:28 -0800

    Changes made

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - to return the factorial of a given number.
+ * @n: number
+ *
+ * Return: always 0 (success)
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1); //Error: invalid argument
+	}
+	else if (n == 0)
+	{
+		return (1);// base case.
+	}
+	else
+	{
+		return (n * factorial(n - 1)); // Recursive case
+	}
+}
