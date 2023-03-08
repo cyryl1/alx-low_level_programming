29a8e8ea7283f7c6e6e3ffda83ae1f4c08ff24fb
Wise1m <aribisalapraise12@gmail.com>
2023-03-08 11:49:38 -0800

    Changes made

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - to return the value of x raised
+ * to power of y.
+ * @x: normal number or base number
+ * @y: power number
+ *
+ * Return: always 0 (sucess)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1); /*error*/
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y-1));
+	}
+}
