10ecd3607585f9c0d8df900d2a199bb1ff1331b2
Wise1m <aribisalapraise12@gmail.com>
2023-03-08 13:39:40 -0800

    Changes made

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,12 +10,17 @@ int is_palindrome(char *s)
 {
 	int len = strlen(s);
 
-	if (len <= 1)
+	if (len == 0 || len == 1)
 	{
 		return (1);
 	}
+
+	if (s[0] == s[len - 1])
+	{
+		return (is_palindrome(s + 1) && is_palindrome(s + len - 2));
+	}
 	else
 	{
-		return (is_palindrome_helper(s, 0, len - 1));
+		return (0);
 	}
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,7 +5,6 @@
 int _putchar(char c);
 int _sqrt_helper(int n, int guess);
 int is_prime_helper(int n, int i);
-int is_palindrome_helper(char *s, int start, int end);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
