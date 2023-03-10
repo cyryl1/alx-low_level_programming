6d862b1be76a932cedb22ea7dac8116674b134fe
Wise1m <aribisalapraise12@gmail.com>
2023-03-10 09:27:46 -0800

    Argument committed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,21 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int i, sum = 0;
 
-	if (argc ==  1)
+	if (argc <  1)
 		printf("0\n");
 		return (0);
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (!atoi(argv[i]))
 		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("%s\n", "Error");
+			return (1);
 		}
 		sum += atoi(argv[i]);
 	}
