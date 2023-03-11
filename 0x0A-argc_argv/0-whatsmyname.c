802754ac794cabb9b89d186a564b69c9465d3107
Wise1m <aribisalapraise12@gmail.com>
2023-03-10 23:19:51 -0800

    Argument committed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,17 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	/*get the program name fom the command-line argument*/
-	char *program_name = argv[0];
-	/*find the last occurrence of '/' in the program name*/
-	char *last_slash = strrchrr(program_name, '/');
+	argc = 0;
 
-	/*if a slash is found, move the pinter forward one character*/
-	if (last_slash != NULL)
-		program_name = last_slash + 1;
-
-	/*print the program name*/
-	printf("%s\n", program_name);
-
+	printf("%s\n", argv[argc]);
 	return (0);
 }
