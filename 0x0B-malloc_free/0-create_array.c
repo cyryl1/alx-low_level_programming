562dcb88943b42e6f72c28770a4f4dc526f79d21
Wise1m <aribisalapraise12@gmail.com>
2023-03-13 04:58:34 -0700

    File committed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,22 +12,20 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr;
-	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	*arr = (char *) malloc(size * sizeof(char));
+	char *arr = (char *) malloc(size * sizeof(char));
 
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
