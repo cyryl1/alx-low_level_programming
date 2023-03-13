793d7029d51410e617d7410f590c47857ad9a87a
Wise1m <aribisalapraise12@gmail.com>
2023-03-13 05:17:43 -0700

    File committed

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - to return a pointer to a new string
+ * and duplicte of the string
+ * 
+ * @str: the string to be duplicated
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dup_str;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	dup_str = (char *) malloc((strlen(str) + 1) * * sizeof(char));
+
+	if (dup_str == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(dup_str, str);
+
+	return dup_str;
+}
