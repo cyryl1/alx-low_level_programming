27773abbaafc4d22cefbf28358d96bf490b97ee1
Wise1m <aribisalapraise12@gmail.com>
2023-03-13 05:56:20 -0700

    File committed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	
+
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	result = malloc(len1 + len2 + 1);
@@ -32,5 +32,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 	memcpy(result, s1, len1);
-	memcpy(result + len1, s2, len2 + 1);return (result);
+	memcpy(result + len1, s2, len2 + 1);
+	
+	return (result);
 }
