5e7d4948f4d0c593aa08eff81b73cceef67238a2
Wise1m <aribisalapraise12@gmail.com>
2023-03-16 04:46:11 -0700

    File committed

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * malloc_checked - to allocate memory using malloc.
+ * @b: used to allocate memory
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		printf("malloc failed: %s\n", strerror(errno));
+		exit(98);
+	}
+	return p;
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+
 int _putchar(char c);
 int putchar(int c);
 void *malloc_checked(unsigned int b);
