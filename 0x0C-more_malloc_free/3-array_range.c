4b9be8a31683d402edfec53b8f41c91a6fc4abe4
Wise1m <aribisalapraise12@gmail.com>
2023-03-16 05:59:41 -0700

    File committed

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,19 +8,23 @@
  */
 int *array_range(int min, int max)
 {
-	int size = max - min + 1;
-	int* arr = (int*) malloc(size * sizeof(int));
+	int size, i;
+	int *arr;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
+
+	size = max - min + 1;
+	arr = malloc(sizeof(int) * size);
+
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		arr[i] = min + i;
 	}
