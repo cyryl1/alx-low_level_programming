c1aa05d2161316d876cdc165e7c50c988f0dc799
Wise1m <aribisalapraise12@gmail.com>
2023-03-16 05:18:44 -0700

    File committed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,40 +9,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, k = 0, j = 0, l = 0;
-	char *str;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
+	char *concat;
 
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 = NULL)
+	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[i])
-		i++;
-	while (s2[k])
-		k++;
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
+	if (n >= len2)
+		n = len2;
+
+
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
 		return (NULL);
 
-	k=0;
-	while (j < l)
-	{
-		if (j <= i)
-			str[j] = s1[j];
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
 
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
-	}
-	str[j] = '\0';
-	return (str);
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
+
+	concat[i + j] = '\0';
+
+	return (concat);
 }
