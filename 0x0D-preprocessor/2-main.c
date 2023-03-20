84c248b3d827b5cac19f02475f5d9a2a6927b83d
Wise1m <aribisalapraise12@gmail.com>
2023-03-20 03:39:18 -0700

    A new commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - to print the file fom which it was compiled from;
+ * return: void
+ */
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
