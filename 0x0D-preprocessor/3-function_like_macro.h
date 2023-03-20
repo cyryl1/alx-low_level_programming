8926af0d1e523204cbae3f1f10f468ee899ab423
Wise1m <aribisalapraise12@gmail.com>
2023-03-20 03:45:34 -0700

    A new commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main: computes the absolute value of a number x.
+ *
+ * Return: void
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+int main(void)
+{
+	int a = -5;
+	double b = -3.14159;
+	printf("ABS(%d) = %d\n", a, ABS(a));
+	printf("ABS(%.2f) = %.2f\n", b, ABS(b));
+	return 0;
+}
