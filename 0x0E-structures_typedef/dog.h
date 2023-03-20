873052816a5f337c3e20cb072cb1d418f561359d
Wise1m <aribisalapraise12@gmail.com>
2023-03-20 04:42:18 -0700

    A new commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - characteristics of a dog
  * @name: name of dog
  * @age: dog's age
  * @owner: owner's name
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+
+#endif /*DOG_H*/
