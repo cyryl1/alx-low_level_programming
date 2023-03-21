768d78ee9a8bbaa640ea078a99960192cd86d165
Wise1m <aribisalapraise12@gmail.com>
2023-03-21 13:09:58 -0700

    A new commit

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,14 +1,6 @@
-#include <stdio.h>
-/**
- * main -  that computes the sum of the numbers x and y.
- *
- * Return: void
- */
+#ifndef SUM_H
+#define SUM_H
+
 #define SUM(x, y) ((x) + (y))
 
-int main(void)
-{
-	int x, y;
-	printf("SUM(%i, %i) = %i\n", x, y, SUM(x, y));
-	return (0);
-}
+#endif /* SUM_H */
