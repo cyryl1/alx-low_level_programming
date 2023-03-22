9a6422f4310b12958293bde793e2cff738d7cce4
Wise1m <aribisalapraise12@gmail.com>
2023-03-22 06:16:37 -0700

    File commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,50 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
+#include <string.h>
+
 /**
- * main - get_op_func has operators correlated with
- * func signs and funcs from op_func
- * if not 4 arguments, return op_func
- * if op is null, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * run calc, input one, operator, input two = pointer res to get_op
- * @argc: arguments
- * @argv: double pointer to arguments
- * Return: 0
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
  */
+
 int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	char *sign;
+	char operator;
+	int a, b, answer;
+	int (*ptr)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
-
-	/* added edge case if argv[2] was longer than 1 char*/
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	operator = argv[2][0];
+	if ((operator != '*' && operator != '+' && operator != '-' && operator != '/' && operator != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if ((operator == '/' || operator == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	res = get_op_func(get_op);
-	ans = res(one, two);
-
-	printf("%d\n", ans);
+	sign = argv[2];
+	ptr = get_op_func(sign);
+	answer = (*ptr)(a, b);
+	printf("%d\n", answer);
 	return (0);
-}
+
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,7 +3,7 @@
 #include "3-calc.h"
 
 /**
- * op_add - 5 functions
+ * op_add - function to add
  * @a: input one
  * @b: input two
  * Return: 0
@@ -13,7 +13,7 @@ int op_add(int a, int b)
 	return (a + b);
 }
 /**
- * op_sub - 5 functions
+ * op_sub - function to subtract
  * @a: input one
  * @b: input two
  * Return: 0
@@ -23,7 +23,7 @@ int op_sub(int a, int b)
 	return (a - b);
 }
 /**
- * op_mul - 5 functions
+ * op_mul - function to multiplies
  * @a: input one
  * @b: input two
  * Return: 0
@@ -32,8 +32,20 @@ int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - 5 functions
+ * op_div - function to div
+ * @a: input 1
+ * @b: input 2
+ * Return: division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function to mod
  * @a: input one
  * @b: input two
  * Return: 0
