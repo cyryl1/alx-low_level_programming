ccd5bc6fcb40366cfd679e76ac418c70f87130de
Wise1m <aribisalapraise12@gmail.com>
2023-03-22 06:53:46 -0700

    File commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 typedef __SIZE_TYPE__size;
-int _putchar(char c);
+char _putchar(char c);
+int putchar(int c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array; int size, int (*cmp)(int));
