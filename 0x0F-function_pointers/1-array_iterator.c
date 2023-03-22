d7ea3447bdca07adee139b58f55cbebbab06e1b8
Wise1m <aribisalapraise12@gmail.com>
2023-03-22 05:24:06 -0700

    File commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL)
-		return;
-	if (action == NULL)
-		return;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array != 0 && size >= 1 && action !- 0)
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 }
