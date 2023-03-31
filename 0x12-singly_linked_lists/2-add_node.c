de9606626ae503294123cc15681a21524fa88a64
Wise1m <aribisalapraise12@gmail.com>
2023-03-31 07:35:31 -0700

    Updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,20 +9,36 @@
  */
 list_t *add_node(list_t **head, cont char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *new_node;
+	size_t str_len = 0;
 
-	while (str[len])
-		len++;
+	/*set string length to 0 if its NULL*/
+	if (str == NULL)
+		str_len = 0;
 
-	new = malloc(sizeof(list_t));
-	if (!new)
+	/*count length of string*/
+	while (str[str_len] != '\0')
+		str_len++;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+	/**
+	 * initialize the next addr of new_node
+	 * to NULL if head is NULL, if it's not
+	 * move the first node to the addr of new_node
+	 * ->next hence adding new_node at the beginning
+	 *  of the list
+	*/
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	*head = new_node;
 
 	return (*head);
 }
